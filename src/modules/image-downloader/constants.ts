4507cc6babaeaf6d200cc0967cd6822b87ed9710
Coal <cocowmn@gmail.com>
2025-12-13 16:00:15 -0800

    add image-downloader implementation

---
+++ src/modules/image-downloader/constants.ts
@@ -0,0 +1,46 @@
+export const MIME_TO_EXTENSION_MAP: Record<string, string> = {
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg',
+  'image/png': 'png',
+  'image/gif': 'gif',
+  'image/webp': 'webp',
+  'image/apng': 'apng',
+  'image/avif': 'avif',
+  'image/svg+xml': 'svg',
+  'image/bmp': 'bmp',
+  'image/x-icon': 'ico',
+  'image/vnd.microsoft.icon': 'ico',
+};
+
+export const EXTENSION_TO_MIME_MAP: Record<string, string> = {
+  jpg: 'image/jpeg',
+  jpeg: 'image/jpeg',
+  png: 'image/png',
+  gif: 'image/gif',
+  webp: 'image/webp',
+  apng: 'image/apng',
+  avif: 'image/avif',
+  svg: 'image/svg+xml',
+  bmp: 'image/bmp',
+  ico: 'image/x-icon',
+};
+
+export const CONVERSION_EXTENSIONS = new Set(['webp', 'avif', 'jpg']);
+
+export const KNOWN_IMAGE_EXTENSIONS = new Set([
+  'jpg',
+  'jpeg',
+  'png',
+  'gif',
+  'webp',
+  'apng',
+  'avif',
+  'svg',
+  'bmp',
+  'ico',
+]);
+
+export const STATIC_IMAGE_EXTENSIONS = new Set(['jpg', 'jpeg', 'png', 'bmp', 'ico']);
+
+export const ANIMATED_FORMATS: string[] = ['webp', 'gif', 'apng', 'avif'];
+export const URL_IMAGE_FORMAT_KEYS: string[] = ['format', 'fm', 'type', 'imageformat', 'ext'];
--- src/modules/image-downloader/types.ts
+++ src/modules/image-downloader/types.ts
@@ -1,1 +1,26 @@
-// Module-specific types go here
+import type { Nullable } from '$utils';
+
+export type ImageURL = `http://${string}` | `https://${string}` | `/${string}` | (string & {});
+
+export type DownloadableImage = HTMLImageElement | HTMLCanvasElement | SVGSVGElement | ImageURL;
+
+export type FilenameGetter = (self?: DownloadableImage, params?: any) => string;
+export interface DownloadImageOptions {
+  filename?: string | FilenameGetter;
+  preferNetwork?: boolean;
+  preferCanvas?: boolean;
+  convert?: 'webp' | 'avif' | 'jpg';
+  downloadTimeoutMS?: number;
+  params?: any;
+}
+export type NormalizedDownloadOptions = Omit<DownloadImageOptions, 'filename' | 'convert'> & {
+  filename?: string;
+  convertMime?: string;
+};
+
+export type ImageMetadata = {
+  src: string;
+  name?: Nullable<string>;
+  extension?: Nullable<string>;
+  mimeType?: Nullable<string>;
+};
